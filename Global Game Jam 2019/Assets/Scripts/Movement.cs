f12d97f5f5f2e5a6b749ddaef8c64d2d0629e70b
Conan Truong <truong.conan@gmail.com>
2019-01-25 23:40:10 -0800

    Create basic movement script

---
+++ Global Game Jam 2019/Assets/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Movement : MonoBehaviour
+{
+    Rigidbody2D rigidBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+    
+    void FixedUpdate()
+    {
+        Vector2 direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * 20f;
+
+        rigidBody.AddForce(direction);
+    }
+}
