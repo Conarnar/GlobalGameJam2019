cb1f83152b15aac4a0edeabf8ea98cc0c76fda79
Conan Truong <truong.conan@gmail.com>
2019-01-26 12:29:49 -0800

    Door and stairs scripts

---
+++ Global Game Jam 2019/Assets/Scripts/Door.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Door : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public Vector3Int position;
+    public TileBase[] doorTiles = new TileBase[2];
+    public int lockId = 0;
+
+    public static List<int> keys = new List<int>();
+
+    void Update()
+    {
+        foreach (Rigidbody2D rigidbody in GetComponentsInChildren<Rigidbody2D>())
+        {
+            rigidbody.simulated = !Unlocked();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (Unlocked())
+            tilemap.SetTile(position, doorTiles[1]);
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (Unlocked())
+            tilemap.SetTile(position, doorTiles[0]);
+    }
+
+    bool Unlocked()
+    {
+        return lockId == 0 || keys.Contains(lockId);
+    }
+}
